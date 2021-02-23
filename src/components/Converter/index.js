9841dbb720af272b1c57cc220e7aa2e71df0949c
colby <colby88cardell@gmail.com>
2021-02-22 20:49:02 -0500

    :construction: Added handlers/Utilized props to render inputs

--- src/components/Converter/index.js
+++ src/components/Converter/index.js
@@ -1,3 +1,46 @@
-const Converter = () => <main></main>;
+import { useState } from "react";
+import { Input } from "./Input";
+import convert from "./lib";
+
+const Converter = () => {
+  const [celsius, setCelsius] = useState(0);
+  const [fahrenheit, setFahrenheit] = useState(0);
+
+  const inputs = [
+    {
+      label: "°C",
+      id: "celsius",
+      value: celsius,
+    },
+    {
+      label: "°F",
+      id: "fahrenheit",
+      value: fahrenheit,
+    },
+  ];
+
+  const inputHandler = ({ target }) => {
+    if (target.id === "celsius") {
+      setCelsius(Number(target.value));
+      setFahrenheit(convert(target.value, "toFahrenheit"));
+    } else {
+      setFahrenheit(Number(target.value));
+      setCelsius(convert(target.value, "toCelsius"));
+    }
+  };
+
+  const renderInputs = () =>
+    inputs.map(({ id, label, value }) => (
+      <Input
+        label={label}
+        id={id}
+        handler={inputHandler}
+        value={value}
+        key={id}
+      />
+    ));
+
+  return <main>{renderInputs()}</main>;
+};
 
 export default Converter;
---
+++ src/components/Converter/input.js
@@ -0,0 +1,28 @@
+import PropTypes from "prop-types";
+
+//functional component
+const Input = ({ label, id, handler, value }) => {
+  const handleChange = (event) => {
+    handler(event);
+  };
+  return (
+    <div>
+      <label>{label}</label>
+      <input
+        placeholder={label}
+        id={id}
+        onChange={handleChange}
+        value={value}
+      />
+    </div>
+  );
+};
+
+Input.propTypes = {
+  handler: PropTypes.func,
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  value: PropTypes.number,
+};
+
+export default Input;
